0d9ef828b8733e8b2e8eaf85f73a886f0ea02cd1
iappssys-user <iappssys-user@apple.com>
2022-09-14 17:48:30 -0600

    creating ejercicio3.9 DynamoDB

---
+++ dia3/ejercicio3.9/main.tf
@@ -0,0 +1,22 @@
+resource "aws_dynamodb_table" "cars" { 
+    name = "cars"
+    hash_key = "VIN"
+    billing_mode = "PAY_PER_REQUEST"
+    attribute {
+        name = "VIN"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table_item" "car-items" {
+  table_name = aws_dynamodb_table.cars.name
+  hash_key = aws_dynamodb_table.cars.hash_key
+  item = <<EOF
+    {
+        "Manufacturer": {"S": "Toyota"},
+        "Make": {"S": "Corolla"},
+        "Year": {"N": "2004"},
+        "VIN": {"S": "4YSL16Z841193"}
+    }
+  EOF
+}

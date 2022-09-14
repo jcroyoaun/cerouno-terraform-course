f3405ead26aec896f75e8a20a56afbed9d6fe843
iappssys-user <iappssys-user@apple.com>
2022-09-13 18:03:22 -0600

    committing ejercicio2.9

---
+++ dia2/ejercicio2.9/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "My Server"
+  }
+}
+
+
+output "instance_ip_addr" {
+  value       = aws_instance.app_server.private_ip
+  description = "The private IP address of the main server instance."
+}

6fddded65dda1903294a9e1fd3d41995d818fa40
iappssys-user <iappssys-user@apple.com>
2022-09-13 18:01:58 -0600

    adding ejercicio2.8

---
+++ dia2/ejercicio2.8/variables.tf
@@ -0,0 +1,19 @@
+variable "filename" {
+  default = "./mascota.txt"
+}
+
+variable "content" {
+  default = ""
+}
+
+variable "prefix" {
+    default = "Mr."
+}
+
+variable "separator" {
+    default = " "
+}
+
+variable "length" {
+    default=1
+}

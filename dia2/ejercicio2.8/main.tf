e78fa3a0077d4fbead7af40828746115735f6e48
Juan Carlos Martinez Carrillo <jcroyoaun@gmail.com>
2023-02-14 18:42:28 -0600

    Create main.tf

---
+++ dia2/ejercicio2.8/main.tf
@@ -0,0 +1,10 @@
+resource "local_file" "mascota" {
+    filename = var.filename
+    content = var.content
+}
+
+resource "random_pet" "my-pet" {
+    prefix = var.prefix
+    separator = var.separator
+    length = var.length
+}

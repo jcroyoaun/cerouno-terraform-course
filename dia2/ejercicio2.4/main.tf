1d4d2a0cad949eec791784323f767cbf67566480
Juan Carlos Martinez Carrillo <jcroyoaun@gmail.com>
2023-02-14 18:05:57 -0600

    Create main.tf

---
+++ dia2/ejercicio2.4/main.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_user" "admin-user" {
+	name = ""
+	tags = {
+		Description = ""
+	}
+}
+
+
+## Ejercicio 2.4.1, expandir variables en el resource block y correr init, plan, apply
+## Terraform import no funciona... arreglemoslo.
+variable "student_role" {
+    default = ""
+}
+
+variable "student_name" {
+    default = ""
+}
